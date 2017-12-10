a2a776d4873a85fcd9470a871c34e110e1059c22
Mira Estil <mira@Miras-MacBook-Pro.local>
2017-12-10 17:23:46 -0500

    testing var

--- Sequencing.java
+++ Sequencing.java
@@ -3,14 +3,16 @@ import java.util.Scanner;
 public class Sequencing {
 	public static void main( String[] args ) {
 		Scanner keyboard = new Scanner(System.in);
-		double price = 0, salesTax, total;
+		double price, salesTax, total;
 
-		salesTax = price * 0.0825;
+		//salesTax = price * 0.0825;
 		// VARIABLES CANNOT HOLD FORUMLAS
-		total = price + salesTax;
+		//total = price + salesTax;
 
 		System.out.print( "How much is the purchase price? " );
 		price = keyboard.nextDouble();
+		salesTax = price * 0.0825;
+		total = price + salesTax;
 
 		System.out.println( "Item price:\t" + price );
 		System.out.println( "Sales tax:\t" + salesTax );
