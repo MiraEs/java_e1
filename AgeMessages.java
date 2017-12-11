3de8ea072a5e0d12a4f189194981f807890deefb
Mira Estil <mira@Miras-MacBook-Pro.local>
2017-12-10 19:54:36 -0500

    testing conditionals

---
+++ AgeMessages.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class AgeMessages {
+	public static void main( String[] args ) {
+		Scanner keyboard = new Scanner(System.in);
+		int age;
+
+		System.out.print( "How old are you? " );
+		age = keyboard.nextInt();
+
+		System.out.println( "You are: " );
+		if ( age < 13 ) {
+			System.out.println( "\ttoo young to create a Facebook account" );
+		} else {
+			System.out.println( "Yay");
+		}
+	}
+}
